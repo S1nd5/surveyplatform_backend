0eba7fb8a7469a04c5eb94269cfca2e50386cf53
Reetta <reetta.meskanen@gmail.com>
2021-11-02 00:22:00 +0200

    pohja projekti

---
+++ src/test/java/hh/swd4/surveyplatform/SurveyplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.swd4.surveyplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
