7fd86d68e32f4bb680b927f83b6ca06d50964975
Santtu Lehtokari <35769108+S1nd5@users.noreply.github.com>
2021-11-23 14:57:20 +0200

    Korjattu workflow config, applikaatio testi

--- src/test/java/hh/swd4/surveyplatform/SurveyplatformApplicationTests.java
+++ src/test/java/hh/swd4/surveyplatform/SurveyplatformApplicationTests.java
@@ -1,13 +1,21 @@
 package hh.swd4.surveyplatform;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
+
+import hh.swd4.surveyplatform.web.AnswerController;
 
 @SpringBootTest
 class SurveyplatformApplicationTests {
-
+	
+	@Autowired
+	private AnswerController aController;
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(aController).getClass();
 	}
 
 }
