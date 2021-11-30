df0f78c91b3cd6a67475ce72acd21825757904db
Santtu Lehtokari <35769108+S1nd5@users.noreply.github.com>
2021-11-30 16:03:59 +0200

    Vastausten käsittelyyn respondent-entiteettiin liittyviä muutoksia

--- src/main/java/hh/swd4/surveyplatform/web/AnswerController.java
+++ src/main/java/hh/swd4/surveyplatform/web/AnswerController.java
@@ -69,10 +69,10 @@ public class AnswerController {
 			return new ResponseEntity<String>("Failed, please provide valid data", HttpStatus.BAD_REQUEST);
 		}
 		
-		String respondentName = obj.getJSONObject("answers").getString("respondent");
+		/*String respondentName = obj.getJSONObject("answers").getString("respondent");
 		if ( respondentName.isEmpty()) {
 			return new ResponseEntity<String>("Failed, please provide valid data (respondent name missing)", HttpStatus.BAD_REQUEST);
-		}
+		}*/
 		
 		JSONArray arr = obj.getJSONObject("answers").getJSONArray("data");
 		if ( arr.isNull(0)) {
@@ -81,8 +81,7 @@ public class AnswerController {
 		
 		try {
 			
-			String[] name = respondentName.split(" ");
-			Respondent respondent = new Respondent(name[0], name[1]);
+			Respondent respondent = new Respondent();
 			respondentRepository.save(respondent);
 			
 			for ( int i = 0; i < arr.length(); i++) {
@@ -100,6 +99,16 @@ public class AnswerController {
 					Survey thisSurvey = s.get();
 					Question thisQuestion = q.get();
 					
+					/* Poikkeus: tsekkaa jos kysymystyyppi on nimi ja kerää tiedot talteen */
+					
+					if ( thisQuestion.getQ_type().equals("fullname")) {
+						if ( answerObj.getString("answer1").isEmpty() != true) {
+							String[] name = answerObj.getString("answer1").split(" ");
+							respondent.setFirstname(name[0]);
+							respondent.setLastname(name[1]);
+						}
+					}
+					
 					String answer1 = answerObj.getString("answer1");
 					String answer2 = answerObj.getString("answer2");
 					String answer3 = answerObj.getString("answer3");
@@ -108,6 +117,7 @@ public class AnswerController {
 					Answer newAnswer = new Answer(thisQuestion, thisSurvey, respondent, answer1, answer2, answer3, answer4);
 					respondent.setSurvey(thisSurvey);
 					
+					respondentRepository.save(respondent);
 					answerRepository.save(newAnswer);
 					createdAnswers.add(newAnswer);
 				}
